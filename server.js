bf08ee392f5d9bf2574a7f3a043a5add23d4b645
vanipriya_piramanathan <vpriyaarun23@gmail.com>
2023-01-07 15:18:29 +0000

    change port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs=require("fs");
 app.use(express.json());
 app.use(cors())
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const routes={
     "/pharmacies":"returns pharmacies list for stratford", 
   "/colleges" : "returns colleges list for stratford",
